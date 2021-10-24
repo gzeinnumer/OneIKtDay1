a829aaed542420338d8d0bf4bd484e0287df2ebb
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-10-25 01:27:15 +0700

    24 -31

--- app/src/main/java/com/gzeinnumer/oneiktday1/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/oneiktday1/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.gzeinnumer.oneiktday1
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.viewbinding.library.activity.viewBinding
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.gzeinnumer.oneiktday1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //todo 26
     private val binding: ActivityMainBinding by viewBinding()
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //todo 28
 //        setContentView(R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
@@ -27,7 +28,20 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
+        //todo 31
+        binding.navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                destination.id.toString()
+            }
+
+            Toast.makeText(this@MainActivity, "Navigated to $dest",Toast.LENGTH_SHORT).show()
+        }
+        //end todo 31
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        //end todo 28
     }
 }

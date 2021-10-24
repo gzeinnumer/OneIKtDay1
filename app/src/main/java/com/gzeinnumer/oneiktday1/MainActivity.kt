8749d351c6baaa4c694c92927f29c28744d12df0
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-10-25 02:05:20 +0700

    32-39

--- app/src/main/java/com/gzeinnumer/oneiktday1/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/oneiktday1/MainActivity.kt
@@ -1,47 +1,62 @@
 package com.gzeinnumer.oneiktday1
 
-import android.content.res.Resources
 import android.os.Bundle
-import android.viewbinding.library.activity.viewBinding
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.gzeinnumer.oneiktday1.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by viewBinding()
+    //todo 39
+//    private val binding: ActivityMainBinding by viewBinding()
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(binding.toolbar)
+        //todo 37 komen this
+//        setSupportActionBar(binding.toolbar)
+//
+//        val host : NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment? ?: return
+//
+//        val navController = host.navController
+//
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//
+//        binding.navView.setupWithNavController(navController)
+//
+//        navController.addOnDestinationChangedListener { _, destination, _ ->
+//            val dest: String = try {
+//                resources.getResourceName(destination.id)
+//            } catch (e: Resources.NotFoundException) {
+//                destination.id.toString()
+//            }
+//
+//            Toast.makeText(this@MainActivity, "Navigated to $dest",Toast.LENGTH_SHORT).show()
+//        }
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        //end todo 37
 
-        val host : NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment? ?: return
-
-        val navController = host.navController
-
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-
-        //todo 31
-        binding.navView.setupWithNavController(navController)
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val dest: String = try {
-                resources.getResourceName(destination.id)
-            } catch (e: Resources.NotFoundException) {
-                destination.id.toString()
-            }
-
-            Toast.makeText(this@MainActivity, "Navigated to $dest",Toast.LENGTH_SHORT).show()
-        }
-        //end todo 31
+        //end todo 38
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.viewTransactionFragment,
+                R.id.chooseRecipientFragment,
+                R.id.viewBalanceFragment
+            )
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        findViewById<BottomNavigationView>(R.id.nav_view).setupWithNavController(navController)
+        //todo 38
     }
 }
--- app/src/main/java/com/gzeinnumer/oneiktday1/fr/MainFragment.kt
+++ app/src/main/java/com/gzeinnumer/oneiktday1/fr/MainFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import com.gzeinnumer.oneiktday1.R
 import com.gzeinnumer.oneiktday1.databinding.FragmentMainBinding
 
@@ -16,29 +14,32 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initView()
-        initOnClick()
+        //todo 33 matikan ini
+//        initView()
+//        initOnClick()
     }
 
-    lateinit var navController: NavController
-
-    private fun initView() {
-        navController = Navigation.findNavController(binding.root)
-    }
-
-    private fun initOnClick() {
-        binding.btnViewTranscation.setOnClickListener {
-            val action = MainFragmentDirections.actionMainFragmentToViewTransactionFragment()
-            navController.navigate(action)
-        }
-        binding.btnSendMoney.setOnClickListener {
-            val action = MainFragmentDirections.actionMainFragmentToChooseRecipientFragment()
-            navController.navigate(action)
-        }
-        binding.btnViewBalance.setOnClickListener {
-            val action = MainFragmentDirections.actionMainFragmentToViewBalanceFragment()
-            navController.navigate(action)
-        }
-    }
+    //todo 34 matikan ini
+//    lateinit var navController: NavController
+//
+//    private fun initView() {
+//        navController = Navigation.findNavController(binding.root)
+//    }
+//
+//    private fun initOnClick() {
+//        binding.btnViewTranscation.setOnClickListener {
+//            val action = MainFragmentDirections.actionMainFragmentToViewTransactionFragment()
+//            navController.navigate(action)
+//        }
+//        binding.btnSendMoney.setOnClickListener {
+//            val action = MainFragmentDirections.actionMainFragmentToChooseRecipientFragment()
+//            navController.navigate(action)
+//        }
+//        binding.btnViewBalance.setOnClickListener {
+//            val action = MainFragmentDirections.actionMainFragmentToViewBalanceFragment()
+//            navController.navigate(action)
+//        }
+//    }
+    //end todo 34
 
 }
